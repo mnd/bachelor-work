89b1a491cef25eb90a8f0ad738efb27d931e166a
Merinov Nikolay <kim.roader@gmail.com>
2010-06-02 03:52:22 +0600

    * library/library.hs: creating repl function * library/Main.hs: using repl function

--- library/Main.hs
+++ library/Main.hs
@@ -1,30 +1,9 @@
 import DynamicFace
-import qualified Simple
-import qualified SimpleTemplate
+import qualified Simple as S
+import qualified SimpleTemplate as ST
 import Monad
 
-main = do
-  l <- getLine
-  if l == "quit" then smain else do
-    cmd <- return $ parseString l
-    res <- return $ cmd >>= eval Simple.symbolTable Simple.readTable
-    s <- return $ res >>= getShow Simple.showTable
-    case s of
-      Just s -> putStrLn s
-      Nothing -> putStrLn "error"
-    main
-    
-smain = do
-  putStrLn "now SimpleTemplate"
-  smain'
-  where
-    smain' = do
-      l <- getLine
-      if l == "quit" then putStrLn "quiting" else do
-        cmd <- return $ parseString l
-        res <- return $ cmd >>= eval SimpleTemplate.symbolTable SimpleTemplate.readTable
-        s <- return $ res >>= getShow SimpleTemplate.showTable
-        case s of
-          Just s -> putStrLn s
-          Nothing -> putStrLn "error"
-        smain'
+main = repl $ SPD [] [] []
+                  [("first", (S.symbolTable, S.readTable, S.showTable)),
+                   ("seconf", (ST.symbolTable, ST.readTable, ST.showTable))]
+                  
--- library/library.hs
+++ library/library.hs
@@ -1,16 +1,16 @@
 module DynamicFace (
   module Data.Dynamic,
-  -- ([Dynamic] -> Maybe Dynamic) -> ([Dynamic] -> Maybe Dynamic) -> [Dynamic] -> Maybe Dynamic
+  -- DFunction -> DFunction -> DFunction
   dplus,                        -- Join two dynamic functions
 
   -- [(String, ([Dynamic] -> Maybe Dynamic))] -> [(String, ([Dynamic] -> Maybe Dynamic))] -> [(String, ([Dynamic] -> Maybe Dynamic))]
-  slplus,                       -- Join two symbol table
+  stplus,                       -- Join two symbol table
 
   -- [(String, (String -> Dynamic))] -> [(String, (String -> Dynamic))] -> [(String, (String -> Dynamic))]
-  rlplus,                       -- Join two read table
+  rtplus,                       -- Join two read table
   
   -- [(TypeRep, (Dynamic -> Maybe String))] -> [(TypeRep, (Dynamic -> Maybe String))] -> [(TypeRep, (Dynamic -> Maybe String))]
-  shlplus,                      -- Join two show table
+  shtplus,                      -- Join two show table
   
   -- [(String, ([Dynamic] -> Maybe Dynamic))] -> String -> Maybe ([Dynamic] -> Maybe Dynamic)
   getFun,                       -- Get function from symbol table by name
@@ -31,7 +31,14 @@ module DynamicFace (
   -- -> [(String, (String -> Dynamic))] -- typeList
   -- -> (String, [(String, String)]) -- (fun, argList)
   -- -> Maybe Dynamic
-  eval                          -- Get result of eval of (functionName, [(ArgRepr, ArgType)]) by symbol table and read table
+  eval,                          -- Get result of eval of (functionName, [(ArgRepr, ArgType)]) by symbol table and read table
+  repl,
+  ProgramData (PD, SPD),
+  readInput,
+  symbolTable,
+  readTable,
+  showTable,
+  importModules
   ) where
 
 import Monad
@@ -40,42 +47,47 @@ import Data.Dynamic
 import Data.Typeable
 import Text.ParserCombinators.Parsec
 
+type DFunction = [Dynamic] -> Maybe Dynamic
+
 -- Собирает две динамические функции в одну
-dplus :: ([Dynamic] -> Maybe Dynamic) -> ([Dynamic] -> Maybe Dynamic) -> [Dynamic] -> Maybe Dynamic
+dplus :: DFunction -> DFunction -> DFunction
 dplus f1 f2 as = f1 as `mplus` f2 as
 
+type SymbolTable = [(String, DFunction)]
+
 -- Объединяет два списка символов
-slplus :: [(String, ([Dynamic] -> Maybe Dynamic))] -> [(String, ([Dynamic] -> Maybe Dynamic))] -> [(String, ([Dynamic] -> Maybe Dynamic))]
-slplus as bs = map slplus' ns
+stplus :: SymbolTable -> SymbolTable -> SymbolTable
+stplus as bs = map stplus' ns
   where 
     ns = nub $ map fst (as ++ bs)
-    slplus' n = case (lookup n as, lookup n bs) of
+    stplus' n = case (lookup n as, lookup n bs) of
       (Just f1, Nothing) -> (n, f1)
       (Nothing, Just f2) -> (n, f2)
       (Just f1, Just f2) -> (n, f1 `dplus` f2)
           
+type ReadTable = [(String, (String -> Dynamic))]                            
+                            
 -- Объединяет два списка типов
-rlplus :: [(String, (String -> Dynamic))] -> [(String, (String -> Dynamic))]
-          -> [(String, (String -> Dynamic))]
-rlplus = (++)
+rtplus :: ReadTable -> ReadTable -> ReadTable
+rtplus = (++)
+
+type ShowTable = [(TypeRep, (Dynamic -> Maybe String))]
 
 -- Объединяет два списка типов выводимых
-shlplus :: [(TypeRep, (Dynamic -> Maybe String))] -> [(TypeRep, (Dynamic -> Maybe String))]
-           -> [(TypeRep, (Dynamic -> Maybe String))]
-shlplus = (++)
-
+shtplus :: ShowTable -> ShowTable -> ShowTable
+shtplus = (++)
 
 -- По имени функции и таблице символов возвращает функцию
-getFun :: [(String, ([Dynamic] -> Maybe Dynamic))] -> String -> Maybe ([Dynamic] -> Maybe Dynamic)
+getFun :: SymbolTable -> String -> Maybe ([Dynamic] -> Maybe Dynamic)
 getFun = flip lookup
 
-getShow :: [(TypeRep, (Dynamic -> Maybe String))] -> Dynamic -> Maybe String
+getShow :: ShowTable -> Dynamic -> Maybe String
 getShow sl a = case lookup (dynTypeRep a) sl of
   Just f -> f a
   Nothing -> Nothing
 
 -- По имени типа и списку типов возвращает функцию чтения
-getRead :: [(String, (String -> Dynamic))] -> String -> Maybe (String -> Dynamic)
+getRead :: ReadTable -> String -> Maybe (String -> Dynamic)
 getRead = flip lookup
 
 ---------------------------- Скопировано из ../process.hs явно стоит оформить как-то отдельной библиотекой -----------
@@ -126,10 +138,10 @@ beforeWord s = do { try (string s)
                   }
                <|> do { c <- anyChar
                       ; (case c of
-                            '\'' -> do{ s0 <- beforeWord "'"
-                                     ; s0' <- beforeWord s
-                                     ; return ((c:s0) ++ ('\'':s0'))
-                                     }
+                            -- '\'' -> do{ s0 <- beforeWord "'"
+                            --          ; s0' <- beforeWord s
+                            --          ; return ((c:s0) ++ ('\'':s0'))
+                            --          }
                             '(' -> do{ s1 <- beforeWord ")"
                                      ; s1' <- beforeWord s
                                      ; return ((c:s1) ++ (')':s1'))
@@ -206,24 +218,76 @@ run p input =
 parseString :: String -> Maybe (String, [(String, String)])
 parseString = run function
 
-argToDyn :: [(String, (String -> Dynamic))] -> (String, String) -> Maybe Dynamic
+argToDyn :: ReadTable -> (String, String) -> Maybe Dynamic
 argToDyn tl (r, t) = ap (getRead tl t) (Just r)
 
 -- По списку разобранной функции, списку символов
 -- и списку типов вычисляет результат
-eval :: [(String, ([Dynamic] -> Maybe Dynamic))] -- symbolList
-        -> [(String, (String -> Dynamic))] -- typeList
+eval :: SymbolTable -> ReadTable
         -> (String, [(String, String)]) -- (fun, argList)
         -> Maybe Dynamic
-
 eval sl tl (f, al) = do
   fun  <- getFun sl f
   args <- sequence $ map (argToDyn tl) al
   fun args
 
-
--- Вопрос дня: Делать явную функцию, которая получает ряд сущностей
--- (таблицу символов, таблицу чтения, таблицу вывода, таблицу модлей, таблицу переменных)
--- или завернуть эти сущности в монаду состояния?
-
 -- Надо написать функцию реализующую цикл чтения, выполнения, вывода.
+
+-- replace old substring with new string in str
+replaceStr :: String -> String -> String -> String
+replaceStr "" old new  = ""
+replaceStr str "" _    = str
+replaceStr str old new = loop str
+  where
+    n = length old
+    loop "" = ""
+    loop str =
+      let (prefix, rest) = splitAt n str
+      in
+        if old == prefix                -- found an occurrence?
+        then new ++ loop rest           -- yes: replace it
+
+        else head str : loop (tail str) -- no: keep looking
+
+
+data ProgramData =
+  PD { readInput :: IO String,
+       symbolTable :: SymbolTable,
+       readTable :: ReadTable,
+       showTable :: ShowTable,
+       importModules :: [(String, (SymbolTable, ReadTable, ShowTable))]
+     }
+  |
+  SPD { symbolTable :: SymbolTable,
+        readTable :: ReadTable,
+        showTable :: ShowTable,
+        importModules :: [(String, (SymbolTable, ReadTable, ShowTable))]
+      }
+                   
+
+repl :: ProgramData -> IO ()
+repl (SPD st rt sht is) = repl (PD getLine st rt sht is)
+repl pd = readInput pd >>= \s1 ->
+  case words s1 of
+    ("quit"):_ -> return ()
+    ("import"):name:_ -> case lookup name (importModules pd) of
+      Just (st, rt, sht) -> do
+        putStrLn "imported"
+        repl pd {
+          symbolTable = symbolTable pd `stplus` st,
+          readTable   = readTable pd `rtplus` rt,
+          showTable   = showTable pd `shtplus` sht
+          }
+      Nothing -> do
+        putStrLn "module not found"
+        repl pd
+    l -> let s' = do
+               cmd <- parseString (unwords l)
+               res <- eval (symbolTable pd)
+                           (readTable pd) cmd
+               getShow (showTable pd) res
+         in do
+          case s' of
+            Just s -> putStrLn s
+            Nothing -> putStrLn "can't parse input"
+          repl pd
